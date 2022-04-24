a2a95e02a75d14ed0b3eb64654c12825baf2aad3
Khanh1234-glitch <85590324+Khanh1234-glitch@users.noreply.github.com>
2022-04-24 16:33:35 +0700

    Update main.js

--- baitapRest/js/main.js
+++ baitapRest/js/main.js
@@ -1,10 +1,27 @@
-const mediumScore = (...numbers) => {
-    let scroreMath = document.getElementById("inpToan").value * 1;
-    console.log(numbers);
+function getEle(id) {
+    return document.getElementById(id);
+}
+
+const getTotal = (...numbers) => {
+    // console.log(numbers);
     let total = 0;
-    for (let i = 0; i < numbers.length; i++) {
+    for (let i in numbers) {
         total += numbers[i];
     }
-    return scroreMath;
-}
-const total = mediumScore();
+    return total / numbers.length;
+};
+
+getEle("btnKhoi1").addEventListener("click", () => {
+    let math = getEle("inpToan").value * 1;
+    let physical = getEle("inpLy").value * 1;
+    let chemistry = getEle("inpHoa").value * 1;
+    getEle("tbKhoi1").innerHTML = getTotal(math, physical, chemistry);
+});
+
+getEle("btnKhoi2").addEventListener("click", () => {
+    let literature = getEle("inpVan").value * 1;
+    let history = getEle("inpSu").value * 1;
+    let geography = getEle("inpDia").value * 1;
+    let english = getEle("inpEnglish").value * 1;
+    getEle("tbKhoi2").innerHTML = getTotal(literature, history, geography, english);
+});
