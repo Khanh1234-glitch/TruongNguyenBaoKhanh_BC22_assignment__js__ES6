e06e745ecc820df2d36c6e8676139f87124313ae
Khanh1234-glitch <85590324+Khanh1234-glitch@users.noreply.github.com>
2022-04-24 16:25:19 +0700

    Update main.js

--- baitapArrowFunc/js/main.js
+++ baitapArrowFunc/js/main.js
@@ -1,38 +1,37 @@
 const colorList = [
-        "pallet",
-        "varidian",
-        "pewter",
-        "cerulean",
-        "vermil",
-        "lion",
-        "lavender",
-        "celadon",
-        "saffron",
-        "fuschia",
-        "cinnabar",
-    ]
-    // { "pallet", "varidian", "pewter", "cerulean", "vermil", "lion", "lavender", "celadon", "saffron", "fuschia", "cinnabar" };
-    // const color = prompt("Pick color");
-    // console.log(colorList[color]);
-for (let value of colorList) {
-    console.log(`value: ${ value }`);
-    const fn = () => {
-        let button1 = "<button onclick='changeColor()' class= 'color-button pallet active '></button>";
-        let button2 = "<button onclick='changeColor()'  class= 'color-button varidian '></button>";
-        let button3 = "<button class= 'color-button pewter'></button>";
-        let button4 = "<button class= 'color-button cerulean'></button>";
-        let button8 = "<button class= 'color-button lavender'></button>";
-        let button9 = "<button class= 'color-button celadon'></button>";
-        let button10 = "<button class= 'color-button saffron'></button>";
-        let button11 = "<button class= 'color-button fuschia'></button>";
-        let button12 = "<button class= 'color-button cinnabar'></button>";
-        document.getElementById("colorContainer").innerHTML = button1 + button2 + button3 + button4 + button8 + button9 + button10 + button11 + button12;
+    "pallet",
+    "viridian",
+    "pewter",
+    "cerulean",
+    "vermillion",
+    "lavender",
+    "celadon",
+    "saffron",
+    "fuschia",
+    "cinnabar",
+];
+
+const colorButton = () => {
+    let content = "";
+    for (let i in colorList) {
+        content += `<button class="color-button ${colorList[i]}"   ></button>`;
     }
-    fn();
+    document.getElementById("colorContainer").innerHTML = content;
+};
+colorButton();
+
+let house = document.getElementById("house");
+let pickColors = document.getElementsByClassName("color-button");
+for (let i = 0; i < pickColors.length; i++) {
+    pickColors[i].addEventListener("click", () => {
+        changeColor(colorList[i], i);
+    });
 }
-const changeColor = () => {
-    document.getElementsByClassName("pallet")[0].addEventListener("click", () => {
-        var result = document.getElementById('house');
-        result.classList.toggleclass('pallet');
-    })
-}
+
+const changeColor = (i, e) => {
+    for (let i = 0; i < pickColors.length; i++) {
+        pickColors[i].classList.remove("active");
+        pickColors[e].classList.add("active");
+    }
+    house.className = "house " + i;
+};
